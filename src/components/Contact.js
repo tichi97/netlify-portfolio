8293244d6e11028b7f3341b7e860b855c92e933d
tichikenani <kenanitichi@gmail.com>
2020-03-26 20:19:07 -0500

    bugs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,7 @@ class Contact extends Component{
             <h1 className='tc pl4'>Contact me</h1>
             <p>Feel free to contact me below. Looking forward to hearing from you!</p>
             <button className='pa3 ba bg-white-90 near-black bg-animate hover-bg-near-black hover-white pointer'>
-            <a href="mailto:kenanitichi@gmail.com" target="_blank" rel="noopener noreferrer">Contact Me</a></button>
+            <a className='link' href="mailto:kenanitichi@gmail.com" target="_blank" rel="noopener noreferrer">Contact Me</a></button>
         </div>
       </Fade>
       </div>
